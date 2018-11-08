4ae8d338423c23c4d9626a5875da8f9bff8ec183
ivanwd7 <ivanwd7@gmail.com>
2018-11-08 16:43:32 +0800

    Countdown Script

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Dec 15, 2018 09:30:01").getTime();
+	var countDownDate = new Date("Dec 15, 2018 09:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
@@ -150,16 +150,16 @@
 
 	// Time calculations for days, hours, minutes and seconds
 	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+	
 	// Display the result in an element with id="demo"
 	// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
 	// + minutes + "Minutes " + seconds + "Seconds ";
 
 	// Display the result in an element with id="demo"
-	document.getElementById("days").innerHTML = days +" <small>days</small>";
+	document.getElementById("days").innerHTML = days + " <small>days</small> ";
 	document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
 	document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
 	document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
